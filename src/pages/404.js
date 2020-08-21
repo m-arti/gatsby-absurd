5e89d94e4f0f380f87396a8acc1d683c64ec1b4d
Martins Samuel <marti.samuel1@gmail.com>
2020-08-21 16:34:25 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,14 @@ import { Container } from '@components/global';
 
 const NotFoundPage = () => (
   <Layout>
-    <Container>
+    <Container style={{marginTop:'20px', textAlign:'left'}}>
       <h1>NOT FOUND</h1>
+      <br></br>
       <p>Sorry, that page does not exist.</p>
+      <br></br>
+      <p>
+        <a style={{color:'#ff5500', textDecoration:'none'}} href="https://martinssamuel.com/">← Homepage</a>
+      </p>
     </Container>
   </Layout>
 );
