49a6706ac113b54dcb1e32c47f4daf0f6930188d
Martins Samuel <marti.samuel1@gmail.com>
2020-09-18 00:21:58 +0100

    Update site

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,12 +6,16 @@ import { Container } from '@components/global';
 const NotFoundPage = () => (
   <Layout>
     <Container style={{marginTop:'20px', textAlign:'left'}}>
-      <h1>NOT FOUND</h1>
+      <hr/>
       <br></br>
-      <p>Sorry, that page does not exist.</p>
+      <h2>L O S T &nbsp; I N &nbsp; S P A C E &nbsp; ✈︎</h2>
       <br></br>
-      <p>
-        <a style={{color:'#ff5500', textDecoration:'none'}} href="https://martinssamuel.com/">← Homepage</a>
+      <br></br>
+      <br></br>
+      <p style={{fontWeight:'300'}}>Sorry, that page does not exist. <br></br> It may have been lost.</p>
+      <br></br>
+      <p style={{fontWeight:'300'}}>
+        <a style={{color:'#ff5500', textDecoration:'none'}} href="https://martinssamuel.com/">← 🌍 Homepage</a>
       </p>
     </Container>
   </Layout>
